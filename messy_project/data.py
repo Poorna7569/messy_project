7d7b50e9d3a0782650a136023602d424ceabaf75
Hemanth kumar kokutam <Hemanthkumar.kokutam@gmail.com>
2026-05-13 17:07:27 +0200

    UPDATE: Changed function name

--- messy_project/data.py
+++ messy_project/data.py
@@ -1,6 +1,6 @@
 import random
 
-def get_data():
+def get_fruit():
     data = ["apple", "banana", "cherry"]
     idx = random.randint(0, len(data) - 1)
     return data[idx]
--- messy_project/logic.py
+++ messy_project/logic.py
@@ -2,7 +2,6 @@ def add_numbers(a, b):
     """Add two numbers."""
     return a + b
 
-
 def subtract_numbers(a, b):
     """Subtract two numbers."""
     return a - b
